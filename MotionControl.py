7e93aa09fa5463ea6d92214d83e136c87131ab35
Thomas Ales <tkales@iastate.edu>
2021-02-24 12:57:01 -0600

    basic control working

--- MotionControl.py
+++ MotionControl.py
@@ -1,8 +1,12 @@
-import zaber_motion
+from typing import NoReturn
+from zaber_motion.ascii import Connection, Device, Axis
+from zaber_motion.units import Units
+from zaber_motion import Library
 
-class MotionControl(self):
+class MotionControl():
 
     def __init__(self):
+        self.serial_connection = []
         self.xy_controller = []
         self.z_controller = []
         self.x_axis = []
@@ -11,4 +15,71 @@ class MotionControl(self):
         self.velocity_target = 0.00
         self.z_offset = 0.00
         
+    def connect_controller(self, _comport):
+        Library.enable_device_db_store()
+        self.serial_connection = Connection.open_serial_port(_comport)
+        self.xy_controller = self.serial_connection.get_device(1)
+        self.z_controller = self.serial_connection.get_device(2)
+        self.xy_controller.identify()
+        self.z_controller.identify()
+        self.x_axis = self.xy_controller.get_axis(1)
+        self.y_axis = self.xy_controller.get_axis(2)
+        self.z_axis = self.z_controller.get_axis(1)
+        return
+
+    def disconnect_controller(self):
+        self.serial_connection.close()
+        return
+
+    def home_x(self):
+        self.x_axis.home()
+        return
+
+    def home_y(self):
+        self.y_axis.home()
+        return
+
+    def home_z(self):
+        self.z_axis.home()
+        return
+
+    def set_z_offset(self):
+        self.z_offset = self.z_axis.get_position()
+        return
+
+    def move_x(self, _position):
+        self.x_axis.move_absolute(_position, Units.LENGTH_MILLIMETRES)
+        self.x_axis.wait_until_idle()
+        return
     
+    def move_y(self, _position):
+        self.y_axis.move_absolute(_position, Units.LENGTH_MILLIMETRES)
+        self.y_axis.wait_until_idle()
+        return
+
+    def move_z(self, _position):
+        self.z_axis.move_absolute(_position, Units.LENGTH_MILLIMETRES)
+        self.z_axis.wait_until_idle()
+        return
+
+    def toggle_feed(self, _state):
+        if _state == True:
+            self.xy_controller.io.set_digital_output(1, True)
+            return
+        elif _state == False:
+            self.xy_controller.io.set_digital_output(1, False)
+            return
+        
+        return
+
+    def toggle_torch(self, _state):
+        if _state == True:
+            self.xy_controller.io.set_digital_output(2, True)
+            return
+        elif _state == False:
+            self.xy_controller.io.set_digital_output(2, False)
+            return
+
+        return
+
+        return
--- __main__.py
+++ __main__.py
@@ -1,6 +1,9 @@
 from PyQt5 import QtWidgets, uic
 
+from serial.tools import list_ports
 import sys
+
+from MotionControl import MotionControl
 
 class Ui(QtWidgets.QMainWindow):
     '''
@@ -24,6 +27,8 @@ class Ui(QtWidgets.QMainWindow):
         self.set_z_level_button.clicked.connect(self.set_z_level)
         self.use_xy_button.clicked.connect(self.set_current_xy)
         self.run_test_button.clicked.connect(self.run_test_fire)
+        self.populate_serial_list()
+        self.motion_controller = MotionControl()
 
     def x_origin_changed(self):
         print("editingFinished event caught on x_origin_le")
@@ -50,7 +55,16 @@ class Ui(QtWidgets.QMainWindow):
         return
 
     def toggle_controller(self):
-        print("controller toggled")
+        print(self.connect_controller_button.text())
+        if self.connect_controller_button.text() == "Connect":
+            print(self.serial_port_combo.currentText())
+            _serport = self.serial_port_combo.currentText().split('(')
+            self.motion_controller.connect_controller(_serport[1][0:4].__str__())
+            self.connect_controller_button.setText('Disconnect')
+        
+        elif self.connect_controller_button.text() == "Disconnect":
+            self.motion_controller.disconnect_controller()
+            self.connect_controller_button.setText('Connect')
         return
     
     def set_z_level(self):
@@ -59,6 +73,10 @@ class Ui(QtWidgets.QMainWindow):
 
     def set_current_xy(self):
         print("current xy clicked")
+        _xpos = float(self.x_origin_le.text())
+        _ypos = float(self.y_origin_le.text())
+        self.motion_controller.move_x(_xpos)
+        self.motion_controller.move_y(_ypos)
         return
 
     def run_test_fire(self):
@@ -67,16 +85,35 @@ class Ui(QtWidgets.QMainWindow):
 
     def home_x(self):
         print("home x")
+        self.motion_controller.home_x()
+        self.motion_controller.x_axis.wait_until_idle()
+        if(self.motion_controller.x_axis.get_position() == 0.0):
+            self.home_x_button.setText('Homed')
+            self.home_x_button.setEnabled(False)
         return
 
     def home_y(self):
         print("home y")
+        self.motion_controller.home_y()
+        self.motion_controller.y_axis.wait_until_idle()
+        if(self.motion_controller.y_axis.get_position() == 0.0):
+            self.home_y_button.setText('Homed')
+            self.home_y_button.setEnabled(False)
         return
 
     def home_z(self):
         print("home z")
+        self.motion_controller.home_z()
+        self.motion_controller.z_axis.wait_until_idle()
+        if(self.motion_controller.z_axis.get_position() == 0.0):
+            self.home_z_button.setText('Homed')
+            self.home_z_button.setEnabled(False)
         return
 
+    def populate_serial_list(self):
+        port_info = list_ports.comports()
+        for x in range(0, port_info.__len__()):
+            self.serial_port_combo.addItem(port_info[x][1])
 
 # Load the Ui and execute the application
 if __name__ == '__main__':
